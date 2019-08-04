3cd6a0b487de28f0af9e8fcb037ea801f4eb67d7
Nic Roland <nicroland9@gmail.com>
2019-08-04 14:02:33 -0700

    rewrote exporter: now records trigger states

--- zabbix_exporter.py
+++ zabbix_exporter.py
@@ -1,38 +1,23 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-import time
-import logging
 import os
-from prometheus_client import start_http_server, Metric
+from prometheus_client import start_http_server, Gauge
 from prometheus_client.core import REGISTRY
 from pyzabbix import ZabbixAPI
-import yaml
-
-
-DEBUG = int(os.environ.get('DEBUG', '0'))
-logger = logging.getLogger(__name__)
 
 
 class ZabbixCollector(object):
-    def __init__(self):
-        pass
+    def __init__(self, url, username, password):
+        zapi = ZabbixAPI(url)
+        zapi.login(username, password)
+
+        # Create metric objs
+        self.trigger_state = Gauge('zabbix_trigger_state', 'Zabbix Trigger value (ok=0, problem=1)')
 
     def collect(self):
-        # define environment variables
-        zabbix_exp_url = os.environ.get('ZABBIX_EXP_URL', 'http://localhost/')
-        zabbix_exp_username = os.environ.get('ZABBIX_EXP_USERNAME', 'Admin')
-        zabbix_exp_password = os.environ.get('ZABBIX_EXP_PASSWORD', 'zabbix')
-
-        zapi = ZabbixAPI(zabbix_exp_url)
-        zapi.login(zabbix_exp_username, zabbix_exp_password)
-
-        # create a prometheus metric
-        metric = Metric('zabbix_warning', 'Current Zabbix Warning Count', 'gauge')
-        # Get a list of all issues (AKA tripped triggers)
-
-        triggers = zapi.trigger.get(only_true=1,
-                                    skipDependent=1,
+        # Get a list of all triggers
+        triggers = zapi.trigger.get(
                                     monitored=1,
                                     active=1,
                                     output='extend',
@@ -40,42 +25,33 @@ class ZabbixCollector(object):
                                     selectHosts=['host'],
                                     )
 
-        # Do another query to find out which issues are Unacknowledged
-        unack_triggers = zapi.trigger.get(only_true=1,
-                                          skipDependent=1,
-                                          monitored=1,
-                                          active=1,
-                                          output='extend',
-                                          expandDescription=1,
-                                          selectHosts=['host'],
-                                          withLastEventUnacknowledged=1,
-                                          )
-
-        unack_trigger_ids = [t['triggerid'] for t in unack_triggers]
-
         for t in triggers:
-            t['unacknowledged'] = True if t['triggerid'] in unack_trigger_ids \
-                else False
+            t['value']
+            for host in t['hosts']:
+                self.trigger_state.set(t['value'], labels={
+                    'host': host['host'],
+                    'trigger': t['description'],
+                    'triggerid': t['triggerid'],
+                    'templateid': t['templateid'],
+                    })
 
-        # Print a list containing only "tripped" triggers
-        # Sum triggers which value is 1
-        warn_cnt = 0
-        for t in triggers:
-            if int(t['value']) == 1:
-                warn_cnt += 1
-
-        # append data to the metric
-        metric.add_sample('zabbix_warning', value=int(warn_cnt), labels={})
-        yield metric
+        yield self.trigger_state
 
 
 def main():
+    # Config as environment variables
+    url = os.environ.get('ZABBIX_EXP_URL', 'http://localhost/')
+    username = os.environ.get('ZABBIX_EXP_USERNAME', 'Admin')
+    password = os.environ.get('ZABBIX_EXP_PASSWORD', 'zabbix')
+
+    collector = ZabbixCollector(url, username, password)
+
     try:
-        # start the webserver on the required port
+        # Start the webserver on the required port
         start_http_server(9288)
-        REGISTRY.register(ZabbixCollector())
+        REGISTRY.register(collector)
     except KeyboardInterrupt:
-        print(" Keyboard Interrupted")
+        print("Keyboard Interrupted")
         exit(0)
 
 
